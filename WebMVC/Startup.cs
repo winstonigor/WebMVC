a3f79e147812213cd15c83dfd5dfb0b85598cd1a
winston igor <winstonigor14@gmail.com>
2020-12-21 21:22:17 -0300

    Criando as Migration

--- WebMVC/Startup.cs
+++ WebMVC/Startup.cs
@@ -37,7 +37,9 @@ namespace WebMVC
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<WebMVCContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("WebMVCContext")));
+            options.UseMySql(Configuration.GetConnectionString("WebMVCContext"), builder => builder.MigrationsAssembly("WebMVC")));
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
